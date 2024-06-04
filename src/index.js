4312b64c37d971d1a59d09a73df1281dd930b472
Xonin-hush <xonindev@gmail.com>
2024-06-04 23:15:48 +0300

    Fetch from weatherapi

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 //linking modules to this main module
-import { out2 } from "./ToDo"
+import { out2 } from "./ToDo";
 import { out } from "./projects";
 
-
+const location= "iraq"
+async function hello() {
+  const myWeather = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=5445bedc62eb468ca61125705240406&q=${location}`,
+    { mode: "cors" }
+  );
+  console.log(myWeather.json());
+}
+hello();
