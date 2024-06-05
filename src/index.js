d41a063e0fea38a86852b514e4409a0b1acf6c78
Xonin-hush <xonindev@gmail.com>
2024-06-05 16:36:54 +0300

    Add input and button to search

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 //linking modules to this main module
+import { add } from "lodash";
 import { out2 } from "./ToDo";
 import { out } from "./projects";
+var location = "mosul";
+const search = document.getElementById("search");
 
-const location= "iraq"
-async function hello() {
+async function fetchWeather() {
+    console.log({location})
   const myWeather = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=5445bedc62eb468ca61125705240406&q=${location}`,
     { mode: "cors" }
   );
   console.log(myWeather.json());
 }
-hello();
+function try1() {
+  search.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    location = document.getElementById("location");
+    console.log(location)
+    location=location.value
+    console.log(location)
+    fetchWeather()
+  });
+}
+try1();
