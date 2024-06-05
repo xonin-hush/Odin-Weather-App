46ab4f710749879f77a243f5a7a8f4c3ae9e370a
xonin-hush <xonindev@gmail.com>
2024-06-05 16:22:53 +0100

    Display info on main page

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import { add } from "lodash";
 import { out2 } from "./ToDo";
 import { out } from "./projects";
 var location = "mosul";
+var tempUnit = "C";
 const search = document.getElementById("search");
 
-async function fetchWeather() {
-  console.log({ location });
-  var myWeather = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=5445bedc62eb468ca61125705240406&q=${location}`,
-    { mode: "cors" }
-  );
-  myWeather = await myWeather.json();
-  console.log(myWeather.current.temp_c);
-}
 function handleSearch() {
   search.addEventListener("click", (event) => {
     event.preventDefault(); // We don't want to submit this fake form
@@ -23,3 +15,35 @@ function handleSearch() {
   });
 }
 handleSearch();
+
+async function fetchWeather() {
+  console.log({ location });
+  var myWeather = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=5445bedc62eb468ca61125705240406&q=${location}`,
+    { mode: "cors" }
+  );
+  myWeather = await myWeather.json();
+  console.log(myWeather);
+  console.log(myWeather.current.temp_c);
+  await displayInfo(myWeather);
+}
+
+function displayInfo(myWeather) {
+  var currentLocation = document.querySelector("#current-location");
+  var temperature = document.querySelector("#temp");
+  var feelsLike = document.querySelector("#feels-like");
+  var pressure = document.querySelector("#pressure");
+  var humidity = document.querySelector("#humidity");
+  currentLocation.textContent = myWeather.location.name;
+
+  if (tempUnit === "C") {
+    temperature.textContent = myWeather.current.temp_c;
+    feelsLike.textContent = myWeather.current.feelslike_c;
+  } else {
+    temperature.textContent = myWeather.current.temp_f;
+    feelsLike.textContent = myWeather.current.feelslike_f;
+  }
+  pressure.textContent = `${myWeather.current.pressure_mb} mb`;
+  humidity.textContent=   `${myWeather.current.humidity}%`
+  console.log("hello", myWeather.current.temp_c);
+}
